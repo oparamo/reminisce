1dd6469450bc4fd3344458e0ad5f767998e1ce86
Oscar Paramo <oscar@vin.li>
2016-08-16 13:34:57 -0500

    Refactor constructor and use bluebird promises.

--- test/index.js
+++ test/index.js
@@ -4,19 +4,22 @@ const expect = require('chai').expect;
 const Reminisce = require('../');
 
 describe('plugin', () => {
-  const testMemories = {
-    test1: { val: 1, ttl: 10000 },
-    test2: { val: 2, ttl: 20000 }
-  };
+  const memories = [
+    { key: 'test1', val: 1, ttl: 10000 },
+    { key: 'test2', val: 2, ttl: 20000 }
+  ];
   let cache;
 
   beforeEach(() => {
-    cache = new Reminisce(testMemories);
+    cache = new Reminisce({
+      memories,
+      options: { ttl: 60000, interval: 30000 }
+    });
   });
 
   describe('constructor', () => {
     it('should initialize with memories', () => {
-      cache = new Reminisce(testMemories);
+      cache = new Reminisce({ memories });
 
       expect(cache).to.be.an('object');
     });
@@ -28,7 +31,9 @@ describe('plugin', () => {
     });
 
     it('should take options', () => {
-      cache = new Reminisce({}, { ttl: 10000, tidyInterval: 60000 });
+      cache = new Reminisce({
+        options: { ttl: 10000, interval: 60000 }
+      });
 
       expect(cache).to.be.an('object');
     });
@@ -38,8 +43,8 @@ describe('plugin', () => {
     it('get a memory', () => {
       return cache.get('test1')
         .then((m) => {
-          expect(m).to.have.property('val', testMemories.test1.val);
-          expect(m).to.have.property('ttl', testMemories.test1.ttl);
+          expect(m).to.have.property('val', 1);
+          expect(m).to.have.property('ttl', 10000);
           expect(m).to.have.property('expires');
         });
     });
