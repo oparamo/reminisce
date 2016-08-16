f18e8ad1346257f94776a4370a47fef1b60cfa51
Oscar Paramo <oscar@vin.li>
2016-08-16 15:38:44 -0500

    Refactor unit tests and increase code coverage.

--- test/index.js
+++ test/index.js
@@ -6,7 +6,7 @@ const Reminisce = require('../');
 describe('plugin', () => {
   const memories = [
     { key: 'test1', val: 1, ttl: 10000 },
-    { key: 'test2', val: 2, ttl: 20000 }
+    { key: 'test2', val: 2 }
   ];
   let cache;
 
@@ -18,6 +18,12 @@ describe('plugin', () => {
   });
 
   describe('constructor', () => {
+    it('should initialize with a single memory', () => {
+      cache = new Reminisce({ memories: memories[0] });
+
+      expect(cache).to.be.an('object');
+    });
+
     it('should initialize with memories', () => {
       cache = new Reminisce({ memories });
 
@@ -40,30 +46,67 @@ describe('plugin', () => {
   });
 
   describe('get memories function', () => {
-    it('get a memory', () => {
+    it('get a single memory', () => {
       return cache.get('test1')
-        .then((m) => {
+        .spread((m) => {
+          expect(m).to.have.property('key', 'test1');
           expect(m).to.have.property('val', 1);
           expect(m).to.have.property('ttl', 10000);
           expect(m).to.have.property('expires');
         });
     });
 
+    it('get multiple memories', () => {
+      return cache.get([ 'test1', 'test2' ])
+        .then((m) => {
+          expect(m).to.have.lengthOf(2);
+          expect(m).to.have.deep.property('[0].key', 'test1');
+          expect(m).to.have.deep.property('[0].val', 1);
+          expect(m).to.have.deep.property('[0].ttl', 10000);
+          expect(m).to.have.deep.property('[0].expires');
+          expect(m).to.have.deep.property('[1].key', 'test2');
+          expect(m).to.have.deep.property('[1].val', 2);
+          expect(m).to.have.deep.property('[1].ttl', 60000);
+          expect(m).to.have.deep.property('[1].expires');
+        });
+    });
+
     it('return undefined if memory does not exist', () => {
       return cache.get('forgot')
-        .then((m) => expect(m).to.be.undefined);
+        .spread((m) => expect(m).to.be.undefined);
     });
   });
 
   describe('set memories function', () => {
-    it('set a memory', () => {
+    it('set a single memory', () => {
       const mem = { key: 'setTest', val: 1, ttl: 10000 };
 
       return cache.set(mem)
-        .then((m) => {
+        .spread((m) => {
+          expect(m).to.have.property('key', mem.key);
           expect(m).to.have.property('val', mem.val);
           expect(m).to.have.property('ttl', mem.ttl);
           expect(m).to.have.property('expires');
+        });
+    });
+
+    it('set multiple memories', () => {
+      const setMemories = [
+        { key: 'setTest1', val: 1, ttl: 10000 },
+        { key: 'setTest2', val: 2 }
+      ];
+
+      return cache.set(setMemories)
+        .then((m) => {
+          expect(m).to.have.lengthOf(2);
+          expect(m).to.have.deep.property('[0].key', 'setTest1');
+          expect(m).to.have.deep.property('[0].val', 1);
+          expect(m).to.have.deep.property('[0].ttl', 10000);
+          expect(m).to.have.deep.property('[0].expires');
+          expect(m).to.have.deep.property('[1].key', 'setTest2');
+          expect(m).to.have.deep.property('[1].val', 2);
+          expect(m).to.have.deep.property('[1].ttl', 60000);
+          expect(m).to.have.deep.property('[1].expires');
         });
     });
 
@@ -71,7 +114,7 @@ describe('plugin', () => {
       const mem = { key: 'test2', val: 1, ttl: 10000 };
 
       return cache.set(mem)
-        .then((m) => {
+        .spread((m) => {
           expect(m).to.have.property('val', mem.val);
           expect(m).to.have.property('ttl', mem.ttl);
           expect(m).to.have.property('expires');
@@ -80,14 +123,23 @@ describe('plugin', () => {
   });
 
   describe('delete memories function', () => {
-    it('should delete a memory', () => {
+    it('should delete a single memory', () => {
       return cache.delete('test2')
-        .then((result) => expect(result).to.be.true);
+        .spread((result) => expect(result).to.be.true);
+    });
+
+    it('should delete multiple memories', () => {
+      return cache.delete([ 'test1', 'test2' ])
+        .then((results) => {
+          expect(results).to.have.lengthOf(2);
+
+          results.forEach((result) => expect(result).to.be.true);
+        });
     });
 
     it('should return true even when deleting non-existing memory', () => {
       return cache.delete('forgot')
-        .then((result) => expect(result).to.be.true);
+        .spread((result) => expect(result).to.be.true);
     });
   });
 });
